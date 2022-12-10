ba947b8c5d3cc5e28e6f91aea3ef69531d89f510
Marika Marszalkowski <marika.marszalkowski@gmail.com>
2022-12-10 10:00:23 +0100

    Shorten

--- 2022/day10/index.ts
+++ 2022/day10/index.ts
@@ -20,28 +20,24 @@ function executeOperations(onCycleUpdate: (cycle: number, x: number) => void) {
   });
 }
 
-function task1() {
-  const interestingCycles = [20, 60, 100, 140, 180, 220];
-  let result = 0;
-  function onCycleUpdate(cycle, x) {
-    result += interestingCycles.includes(cycle) ? cycle * x : 0;
-  }
-  executeOperations(onCycleUpdate);
-  return result;
-}
-
 function drawPixel(cycle: number, signal: number): string {
   const crtPos = cycle % 40;
   const pixel = [signal - 1, signal, signal + 1].includes(crtPos) ? '#' : '.';
   return crtPos ? pixel : `\n${pixel}`;
 }
 
+function task1() {
+  const interestingCycles = [20, 60, 100, 140, 180, 220];
+  let result = 0;
+  executeOperations(
+    (cycle, x) => (result += interestingCycles.includes(cycle) ? cycle * x : 0)
+  );
+  return result;
+}
+
 function task2() {
   let result = '';
-  function onCycleUpdate(cycle, x) {
-    result += drawPixel(cycle, x);
-  }
-  executeOperations(onCycleUpdate);
+  executeOperations((cycle, x) => (result += drawPixel(cycle, x)));
   return result;
 }
 
