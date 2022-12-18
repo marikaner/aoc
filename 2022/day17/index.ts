02ddf558ede409163e48bcb08f4fadf69d19b5c0
Marika Marszalkowski <marika.marszalkowski@gmail.com>
2022-12-18 22:55:28 +0100

    Remove comments

--- 2022/day17/index.ts
+++ 2022/day17/index.ts
@@ -49,21 +49,6 @@ const jets = [...rawJets];
 const emptyLine = [false, false, false, false, false, false, false];
 const lines: boolean[][] = [];
 
-// function move(rock) {
-//   let pos = 2;
-//   for (let i = 0; i < 3; i++) {
-//     pos += jets.shift() === '<' ? -1 : +1;
-//     if (pos < 0) {
-//       pos = 0;
-//     }
-//     if (pos + rock.width > 7) {
-//       pos = 7 - rock.width;
-//     }
-//   }
-
-//   // const findStopLine(rock)
-// }
-
 function isBlockedSide(rock: Rock, xDiff: number): boolean {
   return rock.template.lines.some((rockLine, y) => {
     const line = lines[rock.pos[1] - y];
@@ -71,41 +56,6 @@ function isBlockedSide(rock: Rock, xDiff: number): boolean {
       rock.pos[0] +
       (xDiff < 0 ? rockLine.indexOf(true) : rockLine.lastIndexOf(true));
     return line && line[rockX + xDiff];
-  });
-}
-
-function push(rock: Rock) {
-  if (!jets.length) {
-    jets.push(...rawJets);
-  }
-  const xDiff = jets.shift();
-  // console.log('push', xDiff);
-  const isInBounds =
-    xDiff < 0 ? rock.pos[0] > 0 : rock.pos[0] + rock.template.width < 7;
-  const canPush = isInBounds && !isBlockedSide(rock, xDiff);
-
-  if (canPush) {
-    rock.pos[0] += xDiff;
-    // console.log(rock.pos);
-  } else {
-    // console.log('push blocked');
-  }
-  return canPush;
-}
-
-function rest(rock: Rock) {
-  // console.log('rest');
-  const rockY = rock.pos[1];
-  rock.template.lines.forEach((rockLine, y) => {
-    let totalY = rockY - y;
-    if (!lines[totalY]) {
-      lines.unshift([...emptyLine]);
-      totalY = 0;
-      // lines[totalY] = [...emptyLine];
-    }
-    lines[totalY] = lines[totalY].map(
-      (filled, x) => !!(filled || rockLine[x - rock.pos[0]])
-    );
   });
 }
 
@@ -128,81 +78,50 @@ function isBlockedDown(rock: Rock): boolean {
   });
 }
 
-// function fall(rock): boolean {
-//   let canFall;
-//   if (reachedGround(rock)) {
-//     canFall = false;
-//   } else if (isAboveLines(rock)) {
-//     canFall = true;
-//   } else if (!canFall) {
-//     // canFall = !blocked;
-//     let isBlocked = isBlockedDown(rock);
-//     // if (!blocked) {
-//     //   canFall = true;
-//     //   blocked = newBlocked;
-//     //   // return fall(rock, newBlocked);
-//     // } else {
-//     //   canFall = false;
-//     // }
+function push(rock: Rock) {
+  if (!jets.length) {
+    jets.push(...rawJets);
+  }
+  const xDiff = jets.shift();
+  const isInBounds =
+    xDiff < 0 ? rock.pos[0] > 0 : rock.pos[0] + rock.template.width < 7;
+  const canPush = isInBounds && !isBlockedSide(rock, xDiff);
 
-//     if (isBlocked && !wasPushed) {
-//       console.log('fall blocked');
-//       // lines.unshift([...emptyLine]);
-//       push(rock);
+  if (canPush) {
+    rock.pos[0] += xDiff;
+  }
+}
 
-//       // rock.pos[1] += 1;
-//       isBlocked = isBlockedDown(rock);
-//       // rock.pos[1] -= 1;
-//       canFall = !isBlocked;
-//       // if (!isBlocked)
-//     } else {
-//       canFall = true;
-//     }
-//   }
-
-//   if (canFall) {
-//     rock.pos[1] += 1;
-//     console.log(rock.pos, blocked);
-//     move(rock);
-//   } else {
-//     console.log('fall blocked');
-//     rest(rock);
-//   }
-// }
+function rest(rock: Rock) {
+  const rockY = rock.pos[1];
+  rock.template.lines.forEach((rockLine, y) => {
+    let totalY = rockY - y;
+    if (!lines[totalY]) {
+      lines.unshift([...emptyLine]);
+      totalY = 0;
+    }
+    lines[totalY] = lines[totalY].map(
+      (filled, x) => !!(filled || rockLine[x - rock.pos[0]])
+    );
+  });
+}
 
 function fall(rock: Rock, blocked = false) {
-  const wasPushed = push(rock);
+  push(rock);
 
-  // console.log('fall');
   let canFall;
   if (reachedGround(rock)) {
     canFall = false;
   } else if (isAboveLines(rock)) {
     canFall = true;
   } else if (!canFall) {
-    // canFall = !blocked;
-    let isBlocked = isBlockedDown(rock);
-    // if (!blocked) {
-    //   canFall = true;
-    //   blocked = newBlocked;
-    //   // return fall(rock, newBlocked);
-    // } else {
-    //   canFall = false;
-    // }
-
-    if (isBlocked && wasPushed && false) {
-      // if (!isBlocked)
-    } else {
-      canFall = !isBlocked;
-    }
+    canFall = !isBlockedDown(rock);
   }
 
   if (canFall) {
     rock.pos[1] += 1;
-    // console.log(rock.pos, blocked);
     fall(rock);
   } else {
-    // console.log('fall blocked');
     rest(rock);
   }
 }
@@ -253,7 +172,6 @@ function task2() {
     fall({ template: rock, pos: [2, -4] });
     if (!(i % rocks.length)) {
       probe.push(lines.length - lastLength);
-      console.log(lines.length - lastLength);
       lastLength = lines.length;
     }
   }
