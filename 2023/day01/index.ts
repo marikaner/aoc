b1be310a9e60377cd73ad41b7bfeb7526cb52cf5
Marika Marszalkowski <marika.marszalkowski@gmail.com>
2023-12-01 08:27:46 +0100

    day 1

---
+++ 2023/day01/index.ts
@@ -0,0 +1,73 @@
+import { readInput } from '../read-input.js';
+
+const input = await readInput(import.meta.url);
+const calibrationDocument = input.split('\n');
+
+function task1() {
+  return calibrationDocument
+    .map((line) =>
+      line
+        .split('')
+        .map((char) => parseInt(char))
+        .filter((num) => !Number.isNaN(num))
+    )
+    .reduce((sum, digits) => {
+      const firstDigit = digits[0];
+      const lastDigit = digits[digits.length - 1];
+      return sum + 10 * firstDigit + lastDigit;
+    }, 0);
+}
+
+function task2() {
+  const wordNumbers = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine'
+  ];
+
+  return calibrationDocument
+    .map((line) => {
+      {
+        const numDigits = line
+          .split('')
+          .map((num, i) => [i, parseInt(num)])
+          .filter(([_, val]) => !Number.isNaN(val));
+        const firstNumDigit = numDigits[0];
+        const lastNumDigit = numDigits[numDigits.length - 1];
+
+        const digitsLeft = wordNumbers
+          .map((num, i) => [line.indexOf(num), i + 1])
+          .filter(([index]) => index !== -1);
+        if (firstNumDigit) {
+          digitsLeft.push(firstNumDigit);
+        }
+        const digitsRight = wordNumbers
+          .map((num, i) => [line.lastIndexOf(num), i + 1])
+          .filter(([index]) => index !== -1);
+        if (lastNumDigit) {
+          digitsRight.push(lastNumDigit);
+        }
+
+        const firstDigit = digitsLeft.reduce(
+          (min, left) => (left[0] < min[0] ? left : min),
+          [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+        );
+        const lastDigit = digitsRight.reduce(
+          (max, left) => (left[0] > max[0] ? left : max),
+          [Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]
+        );
+
+        return 10 * firstDigit[1] + lastDigit[1];
+      }
+    })
+    .reduce((sum, num) => sum + num, 0);
+}
+
+console.log(task1());
+console.log(task2());
---
+++ 2023/read-input.ts
@@ -0,0 +1,13 @@
+import { readFile } from 'node:fs/promises';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+export function getDirName(fileUrl: string): string {
+  const __filename = fileURLToPath(fileUrl);
+  const __dirname = dirname(__filename);
+  return __dirname;
+}
+
+export async function readInput(fileUrl: string): Promise<string> {
+  return await readFile(resolve(getDirName(fileUrl), 'input'), 'utf-8');
+}
