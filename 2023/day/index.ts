4fe7e0ca1ba4ef179c5e4ef952b1810b4a8dafb1
Marika Marszalkowski <marika.marszalkowski@gmail.com>
2023-12-13 10:28:30 +0100

    template

---
+++ 2023/day/index.ts
@@ -0,0 +1,12 @@
+import { readInput } from '../read-input.js';
+
+const input = await readInput(import.meta.url);
+
+const something = input.split('\n').map((line) => parseInt(line));
+
+function part1() {}
+
+function part2() {}
+
+console.log(part1());
+console.log(part2());
