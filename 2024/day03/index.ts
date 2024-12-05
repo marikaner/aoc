2b8f5566c979234fa5007925b0d3af9c9001acd2
Marika Marszalkowski <marika.marszalkowski@gmail.com>
2024-12-05 09:16:13 +0100

    wowi

--- 2024/day03/index.ts
+++ 2024/day03/index.ts
@@ -3,11 +3,8 @@ import { sum } from '../util.js';
 
 const input = await readInput(import.meta.url);
 
-const multiplications = [...input.matchAll(/mul\((\d+),(\d+)\)/g)];
-const dos = [...input.matchAll(/do\(\)/g)].map(({ index }) => index);
-const donts = [...input.matchAll(/don't\(\)/g)].map(({ index }) => index);
-
 function part1() {
+  const multiplications = [...input.matchAll(/mul\((\d+),(\d+)\)/g)];
   return sum(
     multiplications.map(
       (multiplication) =>
@@ -17,21 +14,17 @@ function part1() {
 }
 
 function part2() {
-  let doI = 0;
-  let dontI = 0;
-  return multiplications.reduce((sum, multiplication) => {
-    while (multiplication.index > donts[dontI]) {
-      dontI++;
-    }
-    while (multiplication.index > dos[doI]) {
-      doI++;
-    }
-
-    return (sum +=
-      (dos[doI - 1] ?? -1) > (donts[dontI - 1] ?? -2)
-        ? parseInt(multiplication[1]) * parseInt(multiplication[2])
-        : 0);
-  }, 0);
+  const cleanedInput = `do()${input}`
+    .split("don't()")
+    .map((part) => part.slice(part.indexOf('do()')))
+    .join('');
+  const multiplications = [...cleanedInput.matchAll(/mul\((\d+),(\d+)\)/g)];
+  return sum(
+    multiplications.map(
+      (multiplication) =>
+        parseInt(multiplication[1]) * parseInt(multiplication[2])
+    )
+  );
 }
 
 console.log(part1());
