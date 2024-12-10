d75beb087c8a8d94eb1d671a8b24c37a684254b2
Marika Marszalkowski <marika.marszalkowski@gmail.com>
2024-12-10 08:09:05 +0100

    day10

---
+++ 2024/day10/index.ts
@@ -0,0 +1,48 @@
+import { readInput } from '../read-input.js';
+import { sum, unique } from '../util.js';
+
+const input = await readInput(import.meta.url);
+
+const map = input
+  .split('\n')
+  .map((line, y) =>
+    line.split('').map((height, x) => ({ height: parseInt(height), x, y }))
+  );
+
+const heads = map.flat().filter(({ height }) => !height);
+
+type Pos = { x: number; y: number; height: number };
+
+function getNeighbors(pos: Pos) {
+  return [
+    map[pos.y - 1]?.[pos.x],
+    map[pos.y + 1]?.[pos.x],
+    map[pos.y][pos.x - 1],
+    map[pos.y][pos.x + 1]
+  ].filter((p) => p);
+}
+
+function walk(pos: Pos) {
+  if (pos.height === 9) {
+    return [pos];
+  }
+
+  return getNeighbors(pos)
+    .filter(({ height }) => height === pos.height + 1)
+    .flatMap((head) => walk(head));
+}
+
+function part1() {
+  const scores = heads.map(
+    (head) => unique(walk(head).map((top) => JSON.stringify(top))).length
+  );
+  return sum(scores);
+}
+
+function part2() {
+  const ratings = heads.map((head) => walk(head).length);
+  return sum(ratings);
+}
+
+console.log(part1());
+console.log(part2());
